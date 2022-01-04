7cac1c07f9f6309c20645a62c212b1fc0d1bc69a
Donggyu Han <lucky5632@naver.com>
2022-01-04 23:05:39 +0900

    Initial commit

---
+++ src/test/java/Member_Management/Manage/ManageApplicationTests.java
@@ -0,0 +1,13 @@
+package Member_Management.Manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
