a48c1601be5858526e62ff8dfd48de3b04a5497f
Donggyu Han <lucky5632@naver.com>
2022-01-06 21:18:22 +0900

    회원정보 화면 구현

--- src/main/java/Member_Management/Manage/controller/MemberController.java
+++ src/main/java/Member_Management/Manage/controller/MemberController.java
@@ -1,10 +1,14 @@
 package Member_Management.Manage.controller;
 
+import Member_Management.Manage.entity.Member;
 import Member_Management.Manage.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,9 +16,24 @@ public class MemberController {
     private final MemberService memberService;
 
     // 회원리스트 출력
-    @GetMapping("/")
+    @GetMapping("/member/list")
     public String memberList(Model model){
-        model.addAttribute("list", memberService.memberList());
+        List<Member> member = memberService.memberList();
+        model.addAttribute("list", member);
         return "memberlist";
+    }
+
+    // 회원정보 추가
+    @GetMapping("/member/write")
+    public String memberWriteForm(){
+        return "memberwrite";
+    }
+
+    // 회원정보 추가 처리
+    @PostMapping("/member/writing")
+    public String memberWriting(Member member, Model model){
+        memberService.write(member);
+
+        return "redirect:/member/list";
     }
 }
